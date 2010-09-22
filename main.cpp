29f3f6683a323a4da965e8b06ba3a049db30924a
Charles Wang <clwang@csu.fullerton.edu>
2010-09-21 22:44:52 -0700

    An example of reading in a file and allowing a user to input their test case

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 
 using namespace std;
 
 int main(void){
 
+	string file;
+	cout << "Enter your test case file";
+	cin >> file;
 
-
+	ofstream myfile (file);
+	if (myfile.is_open())
+	{
+		myfile << "This is test number 1.\n";
+		myfile << "This is test number 2.\n";
+		myfile.close();
+	}
+	else
+		cout << "Unable to open the file";
 
 
 	return 0;
