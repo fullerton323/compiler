8bbf28c2847b7854640b2243c9c32520abed1d9c
Bryan Perez <perezjbryan@gmail.com>
2010-09-22 15:03:19 -0700

    made function lexer. finished separators.

--- main.cpp
+++ main.cpp
@@ -3,28 +3,40 @@
 #include <string>
 
 
+
+
 using namespace std;
+
+
+string lexer(ifstream &file);
+
 
 int main(void){
 
 	string infile, outfile;
-	cout << "Enter the output file";
+	string tokenANDLexeme;
+
+
+	cout << "Enter the output file ";
 	cin >> outfile;
-	cout << "Enter the input file";
+	cout << "Enter the input file ";
 	cin >>infile;
 
 	ifstream myinfile(infile);
-	outstream myoutfile(outfile);
+	ofstream myoutfile(outfile);
 
 	if (myinfile.is_open())
 	{
 
 		while(!myinfile.eof())
 		{
-
+			tokenANDLexeme = lexer(myinfile);
+			cout<<"Token\t\tLexeme"<<endl;
+			cout<<tokenANDLexeme<<endl;
 		}
 		
 		myinfile.close();
+		myoutfile.close();
 	}
 	else
 		cout << "Unable to open the file";
@@ -32,6 +44,114 @@ int main(void){
 
 	return 0;
 }
+
+
+string lexer(ifstream &file){
+	
+	char c;
+	string lexeme = "", token ="", both="";
+	bool notfound = true;
+
+
+	c = file.get();
+
+	do{
+		
+		switch(c){
+			case '\n':
+				c = file.get();
+				break;
+
+			case ' ':
+				c = file.get();
+				break;
+
+			case '$':
+				lexeme = lexeme + c;
+				c = file.get();
+				if(c=='$'){
+					lexeme = lexeme + c;
+					c = file.get();
+					notfound = false;
+					token = "separator";
+				}
+				break;
+
+			case '{':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case '}':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case ';':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case '(':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case ')':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case ':':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case '[':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+
+			case ']':
+				lexeme = lexeme + c;
+				c = file.get();
+				notfound = false;
+				token = "separator";
+				break;
+			
+			default:
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "unknown";
+				
+		}
+			
+
+	}while(notfound);
+
+	both = token + "\t\t" + lexeme;
+	
+
+	return both;
+
+}
+
+
+
 
 
 
