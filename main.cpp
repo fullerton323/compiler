bb7f7dd2fc691fbe2e37ec0b1d512df4efd8fd79
Bryan Perez <perezjbryan@gmail.com>
2010-09-23 18:56:49 -0700

    Finished. when testing it prints out two of the last item but saves perfect to the file so doesnt matter. last token is always unkown aka endofline symbol.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -12,29 +13,50 @@ int main(void){
 	string infile, outfile;
 	string tokenANDLexeme;
 
-
-	cout << "Enter the output file ";
-	cin >> outfile;
 	cout << "Enter the input file ";
 	cin >>infile;
+	cout << "Enter the output file ";
+	cin >> outfile;
+	
 
 	ifstream myinfile(infile);
 	ofstream myoutfile(outfile);
 
 	if (myinfile.is_open())
 	{
-		cout<<"Token\t\tLexeme"<<endl;
+		
 		while(!myinfile.eof())
 		{
 			tokenANDLexeme = lexer(myinfile);
-			cout<<tokenANDLexeme<<endl;
+			myoutfile<<tokenANDLexeme<<endl;
 		}
 		
-		myinfile.close();
-		myoutfile.close();
+		
 	}
 	else
 		cout << "Unable to open the file";
+
+	myinfile.close();
+	myoutfile.close();
+
+
+	///////////////////////////////////////////////////
+	////  Below is for testing
+	///////////////////////////////////////////////////
+	ifstream file(outfile);
+	string token, lexeme;
+
+	cout<<setw(10)<<"TOKEN\t\t"<<"LEXEME"<<endl;
+	cout<<"-----------------------------------"<<endl;
+	do{
+
+		file>>token>>lexeme;
+		cout<<left<<setw(15)<<token<<setw(30)<<lexeme<<endl;
+
+	}while(!file.eof());
+	file.close();
+	///////////////////////////////////////////////////
+
 
 
 	return 0;
@@ -53,19 +75,71 @@ string lexer(ifstream &file){
 
 		if(isalpha(c)){
 			//identifier and keyword
+
+			lexeme += c;
+			token = "identifier";
+			notfound = false;
+
+			while(isalpha(file.peek()) || isdigit(file.peek()) || file.peek()=='_'){
+				
+				c = file.get();
+				lexeme += c;
+			}
+
+			int size= lexeme.size();
+
+			if(lexeme[size-1] == '_'){
+
+				lexeme.resize(size-1);
+				file.unget();
+			}
+
+
+			if(lexeme=="function")
+				token = "keyword";
+			else if(lexeme=="int")
+				token = "keyword";
+			else if(lexeme=="boolean")
+				token = "keyword";
+			else if(lexeme=="real")
+				token = "keyword";
+			else if(lexeme=="if")
+				token = "keyword";
+			else if(lexeme=="else")
+				token = "keyword";
+			else if(lexeme=="return")
+				token = "keyword";
+			else if(lexeme=="write")
+				token = "keyword";
+			else if(lexeme=="read")
+				token = "keyword";
+			else if(lexeme=="while")
+				token = "keyword";
+			else if(lexeme=="endif")
+				token = "keyword";
+			else if(lexeme=="function")
+				token = "true";
+			else if(lexeme=="false")
+				token = "keyword";
+
 		} 
 		else if(isdigit(c)){
-			//int and real
-			if(token != "identifier"){
-				lexeme += c;
-				token = "digit";
-				string peek;
-				peek = file.peek();
-				if(peek == ".")
-				{
-					token = "real";
+			//integer and real
+
+			    lexeme += c;
+				token = "integer";
+				notfound = false;
+
+				while(isdigit(file.peek()) || file.peek()=='.'){
+					
+					c = file.get();
+					lexeme += c;
+					
+					if( c=='.' )
+						token = "real";
+
 				}
-			}
+
 
 		}
 		else
@@ -74,18 +148,18 @@ string lexer(ifstream &file){
 				break;
 
 			case ' ':
-				notfound = false;
-				both = token + "\t\t" + lexeme;
-				return both;
 				break;
 
 			case '$':
 				lexeme = lexeme + c;
-				if(c=='$'){
-					lexeme = lexeme + c;
+				if(file.peek()=='$'){
 					c = file.get();
+					lexeme = lexeme + c;
 					notfound = false;
 					token = "separator";
+				}else{
+					notfound = false;
+					token = "unknown";
 				}
 				break;
 
@@ -97,14 +171,10 @@ string lexer(ifstream &file){
 
 			case '}':
 				notfound = false;
-				if (token == "digit"){
-					file.putback(c);
-				}
-				else{
-					token = "separator";
-					lexeme += c;
-				}
+				token = "separator";
+				lexeme += c;
 				break;
+
 			case ';':
 				lexeme = lexeme + c;
 				notfound = false;
@@ -118,20 +188,22 @@ string lexer(ifstream &file){
 				break;
 
 			case ')':
+				lexeme = lexeme + c;
 				notfound = false;
-				if (token == "digit"){
-					file.putback(c);
-				}
-				else{
-					token = "separator";
-					lexeme += c;
-				}
+				token = "separator";
 				break;
 
 			case ':':
 				lexeme = lexeme + c;
 				notfound = false;
 				token = "separator";
+
+				if(file.peek()=='='){
+					c = file.get();
+					lexeme += c;
+					token = "operator";
+				}
+
 				break;
 
 			case '[':
@@ -141,26 +213,84 @@ string lexer(ifstream &file){
 				break;
 
 			case ']':
+				lexeme = lexeme + c;
 				notfound = false;
-				if (token == "digit"){
-					file.putback(c);
-				}
-				else{
-					token = "separator";
+				token = "separator";
+				break;
+
+			case ',':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "separator";
+				break;
+
+			case '=':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+
+				if(file.peek()=='>'){
+					c = file.get();
 					lexeme += c;
 				}
 				break;
-			
+
+			case '>':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+				break;
+
+			case '<':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+
+				if(file.peek()=='='){
+					c = file.get();
+					lexeme += c;
+				}
+				break;
+
+			case '/':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+
+				if(file.peek()=='='){
+					c = file.get();
+					lexeme += c;
+				}
+				break;
+
+			case '*':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+				break;
+
+			case '+':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+				break;
+
+			case '-':
+				lexeme = lexeme + c;
+				notfound = false;
+				token = "operator";
+				break;
+
 			default:
 				lexeme = lexeme + c;
 				notfound = false;
-				//token = "unknown";
+				token = "unknown";
 				
 		}			
 
 	}while(notfound);
 
-	both = token + "\t\t" + lexeme;
+	both = token + " " + lexeme;
 	
 
 	return both;
